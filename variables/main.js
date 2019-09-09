076468a9aca7a09ad9226b83712b5bf9f29ba424
Kelakhsaev Aleksandr <aleks903@rambler.ru>
2019-09-09 21:18:14 +0300

    homework 1

---
+++ variables/main.js
@@ -0,0 +1,30 @@
+//задача 1
+console.log('Задача 1\n');
+
+let algebra = 5;
+let geography = 4;
+let physics = 4;
+let averageMark;
+
+averageMark = (algebra + geography + physics) / 3;
+console.log('Средний бал = ' + averageMark);
+
+//задача 2
+console.log('\n\nЗадача 2\n');
+
+let name = 'Александр';
+let message;
+
+message = `Привет, мир! Меня зовут ${name}.`;
+console.log(message);
+
+//задача 3
+console.log('\n\nЗадача 3\n');
+
+let x = 2;
+let y = 22;
+let z = 0;
+let result;
+
+result = (x * y) + (5 * z) + (x - 1);
+console.log(`при x = ${x}, y = ${y}, z = ${z}\n` + '(x * y) + (5 * z) + (x - 1) = ' + result);
