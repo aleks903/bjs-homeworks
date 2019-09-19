eeb8b7ab70861e5362db6117ed4484a75db7e977
Kelakhsaev Aleksandr <aleks903@rambler.ru>
2019-09-19 20:36:49 +0300

    homeworks 2.1

--- wrappers-for-primitives/task.js
+++ wrappers-for-primitives/task.js
@@ -1,18 +1,49 @@
+"use strict";
 function calculateMortgage() {
     let percent = window.percent.value;
     let contribution = window.contribution.value;
     let amount = window.amount.value;
     let date = window.date.value;
 
-    let result = calculateTotalMortgage(percent, contribution, amount, date);
+    let result = calculateTotalMortgage(percent, contribution, amount, amountMonths(date));
     let span = window.mortageResult;
     span.textContent = result;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-
     // код для задачи №1 писать здесь
-    //return totalAmount;
+    if(isNaN(Number(percent)) || percent.length === 0) {
+        alert(`Параметр Процентная ставка содержит неправильное значение - ${percent}`);
+    return;
+    }
+    if(isNaN(Number(contribution)) || contribution.length === 0) {
+        alert(`Параметр Начальный взнос содержит неправильное значение ${contribution}`);
+    return;
+    }
+    if(isNaN(Number(amount)) || amount.length === 0) {
+        alert(`Параметр Общая стоимость содержит неправильное значение ${amount}`);
+    return;
+    }
+
+    let P = ((percent / 100) / 12);
+    let salaryMont = (amount - contribution) * (P + P / ((Math.pow((1 + P), date)) - 1));
+    let totalAmount = (salaryMont * date).toFixed(2);
+    console.log(totalAmount);
+    return totalAmount;    
+}
+
+function checkInputNaN(inputValue) {
+    if(isNaN(Number(inputValue))) {
+        return true;
+    }
+}
+
+function amountMonths(curDate) {
+    let nowDate = new Date;
+    let endDate = new Date(curDate);
+    let months;
+    months = ((endDate.getFullYear() - nowDate.getFullYear()) * 12) + (endDate.getMonth() - nowDate.getMonth());
+    return months;
 }
 
 function sayHello() {
@@ -24,5 +55,13 @@ function sayHello() {
 
 function getGreeting(name) {
     // код для задачи №2 писать здесь
-    //return greeting;
+    let greeting = 'Привет, мир! Меня зовут ';
+    if(name.toLowerCase()!='null' && name.toLowerCase()!='undefined' && name) {
+        greeting += name;
+    }
+    else {
+        greeting += 'Аноним';
+    }
+    console.log(greeting);
+    return greeting;
 }
