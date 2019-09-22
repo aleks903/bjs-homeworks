3999a54c15d173f33c105417417d8723448c42eb
Kelakhsaev Aleksandr <aleks903@rambler.ru>
2019-09-22 21:15:16 +0300

    homeworks 2.2

--- object-prototype/main.js
+++ object-prototype/main.js
@@ -8,6 +8,10 @@ function initCheckBirthday() {
 
 function checkBirthday(birthday) {
     // код для задачи №1 писать здесь
+    let now = Date.now();
+    birthday = Number(new Date(birthday));
+    let diff = (now - birthday) / 31556736000;
+    return (diff > 18);
 }
 
 function initPrintAnimalSound() {
@@ -22,6 +26,12 @@ function initPrintAnimalSound() {
 
 function getAnimalSound(animal) {
     // код для задачи №2 писать здесь
+  if (animal == 'undefined') {
+		return null;
+  }
+  else {
+  	return animal.sound;	
+  }
 }
 
 function initCalculateStatement() {
@@ -36,4 +46,12 @@ function initCalculateStatement() {
 
 function getAverageMark(marks) {
     // код для задачи №3 писать здесь
+
+    let roundedAverage = 0;
+    for (mark of marks) {
+    	roundedAverage += Number(mark);
+    }
+console.log(roundedAverage);
+    roundedAverage = Math.round(roundedAverage / marks.length);
+    return roundedAverage;
 }
